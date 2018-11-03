cced72dfdbc7e090229f3490a82e7920d4e1a06a
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-11-03 00:07:09 +0000

    Added projects model

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IssueTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace IssueTracker.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
---
+++ IssueTracker/Models/ProjectModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IssueTracker.Models
+{
+    public class ProjectModels
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName="varchar")]
+        [MinLength(10)]
+        public string Description { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0}, {1}", this.FirstName, this.LastName);
+            }
+        }
+
+        public virtual ApplicationUser OwnerUser { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        [Required]
+        public string OwnerUserID { get; set; }
+    }
+}
