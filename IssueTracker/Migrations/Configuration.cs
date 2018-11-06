205f744981929089ed4d63d0ea3c0e6a1faceed3
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-11-06 17:25:16 +0000

    completed admin functionality to list all projects, regardless of ownership

--- IssueTracker/Migrations/Configuration.cs
+++ IssueTracker/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace IssueTracker.Migrations
 {
     using IssueTracker.Models;
+    using IssueTracker.Services;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
@@ -8,7 +9,7 @@ namespace IssueTracker.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<IssueTracker.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
@@ -16,7 +17,7 @@ namespace IssueTracker.Migrations
             ContextKey = "IssueTracker.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(IssueTracker.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
             var userStore = new UserStore<ApplicationUser>(context);
@@ -26,6 +27,15 @@ namespace IssueTracker.Migrations
             {
                 var user = new ApplicationUser { UserName = "admin@kanban.net", Email = "admin@kanban.net" };
                 userManager.Create(user, "Password!2");
+
+                var service = new UserAccountService(context);
+                service.CreateUserAccount("admin", "adminson", user.Id);
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges(); // save changes to db to save role for following role asignment
+
+                userManager.AddToRole(user.Id, "Admin");
+                    
             }
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
